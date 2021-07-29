6a3c0aff5e87a241aac37e374d6dc1baf739945c
yangyongming <yangyongming@kuaidihelp.com>
2021-07-29 19:40:20 +0800

    release: v0.0.1-alpha.2 支持配置预下载

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 /**
  * root 文件所在根路径
  *
+ * preloadRule 页面路劲，进入该页面下载分包
+ *
+ * network 现在分包的网络 默认为all
+ *
  * outputRoot "auto" 根据root自动转化， "string" 自定义子包跟路径；默认为auto
  * 例1：outputRoot:"auto"，
  *     打包后页面路径为 `pages-${index}/page/index`
@@ -18,6 +22,8 @@
   subPackages: [
     {
       root: "pages",
+      preloadRule: "pages/index",
+      network: "all",
       outputRoot: "auto|其他自定路径",
       pages: ["address/index"],
     },
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -15,6 +15,21 @@ function readAppJson(ctx) {
 
 /**
  * 
+ * @description 收集预下载分包配置，微信支持配置 name
+ * @param param0 
+ * @param preloadRuleMap 
+ */
+function collectPreloadRule({ preloadRule, name, network = "all" }, preloadRuleMap) {
+  if (!preloadRule) return
+  preloadRuleMap[preloadRule] = preloadRuleMap[preloadRule] || {
+    packages: [],
+    network
+  }
+  preloadRuleMap[preloadRule].packages.push(name);
+}
+
+/**
+ * 
  * @description 移动分包
  */
 export function mvSubPackages(ctx) {
@@ -24,10 +39,14 @@ export function mvSubPackages(ctx) {
     const { outputPath } = ctx.paths;
     const fileType = fileTypeMap[PLATFORM_ENV];
     if (!fileType) return;
-    const movePaths = [];
-    const subPackagesFormatted = subPackages.map(({ root: sourceRoot, pages, outputRoot = "auto", ...subItem }, index) => {
+
+    const movePaths = []; // 分包列表
+    const preloadRuleMap = {}; // 预下载配置
+
+    const subPackagesFormatted = subPackages.map(({ preloadRule, network, root: sourceRoot, pages, outputRoot = "auto", ...subItem }, index) => {
       const subRoot = outputRoot === 'auto' ? `${sourceRoot}-${index}` : outputRoot;
-      // 收集需要移动的文件列表
+
+      // 收集需要移动的分包列表
       pages.map(item => {
         const from = path.resolve(outputPath, `./${sourceRoot}/${item}`);
         const to = path.resolve(outputPath, `./${subRoot}/${item}`);
@@ -36,6 +55,16 @@ export function mvSubPackages(ctx) {
           return [from + suffix, to + suffix];
         }));
       });
+
+      // preloadRule
+      let name = subRoot;
+      if (PLATFORM_ENV === 'weapp') {
+        // 微信可配置name
+        name = subItem.name || subRoot;
+      } else {
+        delete subItem.name;
+      }
+      collectPreloadRule({ preloadRule, name, network }, preloadRuleMap);
       return {
         root: subRoot,
         pages,
@@ -46,10 +75,11 @@ export function mvSubPackages(ctx) {
     // 更改subPackages配置
     writeJson(appJsonPath, {
       ...appCon,
+      preloadRule: preloadRuleMap,
       subPackages: subPackagesFormatted
     });
 
-    // 移动分包到置顶目录
+    // 移动分包到置顶目录，后续考虑是否在分包移动后移除原空文件夹
     movePaths.map(([from, to]) => {
       if (existsSync(from)) {
         moveSync(from, to, { overwrite: true });
@@ -61,7 +91,7 @@ export function mvSubPackages(ctx) {
 }
 
 /**
- * 文件类型后缀
+ * @description 文件类型后缀
  */
 export const fileTypeMap = {
   weapp: {
